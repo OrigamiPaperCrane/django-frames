cac50d799da34e3d0a1637485e82daeac0048ce0
OrigamiPaperCrane <paschulz@mail.de>
2022-05-12 19:25:43 +0200

    add frame package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='django_frames',
+    name='django-frames',
     version='0.1.0',
     description='Use Django entities with pandas data frames',
     url='https://github.com/OrigamiPaperCrane/django-frames',
